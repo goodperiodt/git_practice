bc1141d6a8eca204dbcf28a9fc8433a177fbe357
goodperiodt <ilypsj@gmail.com>
2023-02-20 20:16:41 +0900

    세 번째 커밋 완료

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,6 +5,8 @@ public class GitPracticeRun {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요!");
 		System.out.println("Git 연습용 파일입니다.");
+		
+		System.out.println("github 연동 완료했습니다.");
 	}
 
 }
